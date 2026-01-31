d0b9008068e4f6b53aef2f3cc5b53f4ec06d46a8
Camila Rocha <63120151+CamilaSRocha@users.noreply.github.com>
2026-01-31 17:16:08 -0300

    adicionando script para informar o erro na seleção d foto de capa

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para alternar entre as opções de "claro" e "escuro" na parte de Estilo do convite. Altera as cores do botão switch e o nome ao lado.
 function toggleMode() {
     const html = document.documentElement
     const modeName = document.getElementById("mode-name")
@@ -11,3 +12,31 @@ function toggleMode() {
     }
 }
 
+
+// Fazer com que a div de erro apareça somente quando tiver um erro na seleção da imagem para foto de capa. Mudar o texto que aparece de acordo com o erro (formato errado ou não selecionou imagem).
+const form = document.querySelector("form")
+const fileInput = document.getElementById("cover-photo")
+const errorDiv = document.getElementById("file-error")
+const errorSpan = errorDiv.querySelector("span")
+
+form.addEventListener("submit", function (event) {
+
+    const file = fileInput.files[0]
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"]
+
+    if (!file) {
+        event.preventDefault()
+        errorDiv.style.display = "flex"
+        errorSpan.textContent = "Selecione uma imagem para continuar."
+
+        return
+    } if (!allowedTypes.includes(file.type)) {
+        event.preventDefault()
+        errorDiv.style.display = "flex"
+        errorSpan.textContent = "Formato inválido. Selecione uma imagem."
+
+        return
+    } else {
+        errorDiv.style.display = "none"
+    }
+})
